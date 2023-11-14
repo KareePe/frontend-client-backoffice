56256ad4d96f1434329a2b59998063783b040b75
kuzarandom <peerapat.loeipit@gmail.com>
2023-11-15 00:56:39 +0700

    config fixbug window in not define apex charts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,6 +37,9 @@ export default {
   build: {
     transpile: ["vuetify"],
   },
+  plugins: [
+    { src: '~/plugins/apexcharts.ts', mode: 'client' }
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
